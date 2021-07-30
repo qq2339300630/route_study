23972b168d7992e79fdec877829995603fb08277
曹佳俊 <caojiajun1@shein.com>
2021-07-30 15:57:01 +0800

    路由传值

--- lib/nonepager.dart
+++ lib/nonepager.dart
@@ -7,8 +7,47 @@ class NewRoute extends StatelessWidget {
       appBar: AppBar(
         title: const Text("new route"),
       ),
-      body: const Center(
-        child: Text('This is new route'),
+      body: Center(
+        child: TextButton(
+            child: const Text('This is new route'),
+            onPressed: () async {
+              var result = await Navigator.push(context,
+                  MaterialPageRoute(builder: (context) {
+                return const TipRoute(text: "你好呀");
+              }));
+              print(result);
+            }),
+      ),
+    );
+  }
+}
+
+class TipRoute extends StatelessWidget {
+  final String text;
+
+  const TipRoute({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('提示'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'hello world');
+                },
+                child: const Text('返回'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
